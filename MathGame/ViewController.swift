b8b1cc98e091c9c3d40f6d9e29c395f357d1a7a2
Saeed Rahman <saeedrahman@users.noreply.github.com>
2025-10-02 08:25:17 +0800

    Initial Commit

---
+++ MathGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MathGame
+//
+//  Created by Saeed Rahman on 2/10/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
